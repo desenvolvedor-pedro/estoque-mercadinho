db49b3b3c60430c57be26f8c7d5f4236ab4072d5
Marcelo Figueira <130791829+Marcelo030@users.noreply.github.com>
2024-04-15 10:55:22 -0300

    Update Data.java

--- src/Data.java
+++ src/Data.java
@@ -1,11 +1,42 @@
-//Organização de datas.
 class Data {
-    int dia;
-    int mes;
-    int ano;
+  private int dia;
+  private int mes;
+  private int ano;
 
-    String formatado (){
-        return  this.dia + "/" + this.mes + "/" + this.ano;
+  // Construtor
+  public Data(int dia, int mes, int ano) {
+      this.dia = dia;
+      this.mes = mes;
+      this.ano = ano;
+  }
 
-    }
+  // Métodos get
+  public int getDia() {
+      return dia;
+  }
+
+  public int getMes() {
+      return mes;
+  }
+
+  public int getAno() {
+      return ano;
+  }
+
+  // Métodos set
+  public void setDia(int dia) {
+      this.dia = dia;
+  }
+
+  public void setMes(int mes) {
+      this.mes = mes;
+  }
+
+  public void setAno(int ano) {
+      this.ano = ano;
+  }
+
+  public String formatado() {
+      return this.dia + "/" + this.mes + "/" + this.ano;
+  }
 }
